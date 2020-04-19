dedcefa9883b4b3838608093f888610210428536
Cody <lawdermiltcody@gmail.com>
2020-04-19 02:40:58 -0500

    be able to add user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,30 @@
 class UserController < ApplicationController
+    skip_before_action :verify_authenticity_token, :only => [:new, :create, :index]
+    skip_before_action :authorized, only: [:show, :update, :index, :create]
+
+    def index
+        @users = User.all 
+        render json: @users
+    end
+
+    def new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.save
+            @token = encode_token(user_id: @user.id)
+            render json: { user: @user.as_json, jwt: @token }, status: :created
+          else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+          end
+    end
+
+    private
+
+    def user_params
+        byebug
+        params.required(:user).permit(:username, :password_digest)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post '/signup', to: 'user#create'
   resources :users
   resources :clients
   resources :appointments
