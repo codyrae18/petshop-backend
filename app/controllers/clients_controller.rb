3ba27b5f532360b1e6f073278cdd9c8513356ad7
Cody <lawdermiltcody@gmail.com>
2020-04-22 16:42:51 -0500

    be able to show dogs on a specific client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,10 +1,15 @@
 class ClientsController < ApplicationController
-    skip_before_action :verify_authenticity_token, :only => [:new, :create, :index]
+    skip_before_action :verify_authenticity_token, :only => [:new, :create, :index, :show]
     skip_before_action :authorized, only: [:show, :update, :index, :create]
 
     def index
         @clients = Client.all 
         render json: @clients
+    end
+
+    def show
+        @client = Client.find_by(id: params[:id])
+        render json: @client.dogs
     end
 
     def new
