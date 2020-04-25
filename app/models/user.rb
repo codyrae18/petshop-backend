874eab1f8a2ce1af6863e2d2ff66abf844afffb2
Cody <lawdermiltcody@gmail.com>
2020-04-25 15:33:42 -0500

    be able to add user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,28 @@
 class User < ApplicationRecord
+    skip_before_action :verify_authenticity_token, :only => [:new, :create, :index, :show, :destroy]
+    skip_before_action :authorized, only: [:show, :update, :index, :create, :destroy]
+   
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        byebug
+        @user = User.new(user_params)
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
