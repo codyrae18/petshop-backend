64fae341fffc32d781e13ad81366f350b43865d9
Cody <lawdermiltcody@gmail.com>
2020-04-29 02:06:04 -0500

    services controller and appointments

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,29 @@
 class ServicesController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def new
+
+    end
+
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :appointments
   resources :pets
   resources :breeds
+  resources :services
 end
