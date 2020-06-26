49a7de423544ff813620cada35899c9f22fa4c69
Cody <lawdermiltcody@gmail.com>
2020-06-25 21:45:59 -0500

    breed-controller cleaner

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,5 +1,4 @@
 class BreedsController < ApplicationController
-    # skip_before_action :verify_authenticity_token, :only => [:new, :create, :index]
     # skip_before_action :authorized, only: [:show, :update, :index, :create]
     skip_before_action :authorized, only: [:index, :create, :update]
 
